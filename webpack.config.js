bc1662f755e0c1b2fef0f996505a68d19d7083c5
JOHN NICO <jnico810@gmail.com>
2016-10-18 10:47:23 -0400

    Setup es6 config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,15 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
+    loaders: [
+      {test: [/\.jsx?$/, /\.js?$/],
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015']
+      }
+    }
+    ]
   },
   devtool: 'source-maps',
   resolve: {
